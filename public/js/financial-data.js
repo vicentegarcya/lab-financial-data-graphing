efbaadccfde9f25ad072e36c30a0e25054b65ced
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-02-15 10:14:32 +0100

    finish iteration 3

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,26 +1,45 @@
-axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-    .then((data) => {
-        console.log(data.data.bpi);
-        const dates = Object.keys(data.data.bpi);
-        const bpi = Object.values(data.data.bpi);
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: dates,
-                datasets: [{
-                    label: 'Bitcoin Price Index',
-                    data: bpi,
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.5)',
-                    ],
-                    borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                    ],
-                    borderWidth: 2
-                }]
-            },  
-        });
-        console.log(myChart);
-    })
-    .catch(err => next(err));
+let fromDate = "2021-11-03";
+let toDate = "2022-02-03";
+let myChart;
+
+const loadData = () => {
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`)
+        .then((data) => {
+            const dates = Object.keys(data.data.bpi);
+            const bpi = Object.values(data.data.bpi);
+            const ctx = document.getElementById('myChart').getContext('2d');
+            myChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: dates,
+                    datasets: [{
+                        label: 'Bitcoin Price Index',
+                        data: bpi,
+                        backgroundColor: [
+                            'rgb(152, 251, 152, 0.5)',
+                        ],
+                        borderColor: [
+                            'rgba(152, 251, 152, 1)',
+                        ],
+                        borderWidth: 2,
+                        fill: true
+                    }]
+                },  
+            });
+        })
+        .catch(err => console.log(err));
+}
+
+loadData();
+
+document.getElementById('from-date').addEventListener('change', (event) => {
+    myChart.destroy();
+    fromDate = event.target.value;
+    loadData();
+})
+
+document.getElementById('to-date').addEventListener('change', (event) => {
+    myChart.destroy();
+    toDate = event.target.value;
+    loadData();
+})
